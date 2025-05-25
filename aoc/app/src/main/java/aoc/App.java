f3844616f0b37cf25d1ec01a387770a0baf2d910
jordan <sudo.sendmail.jordan@gmail.com>
2025-05-24 19:33:27 -0700

    Add DayOne B

--- aoc/app/src/main/java/aoc/App.java
+++ aoc/app/src/main/java/aoc/App.java
@@ -1,28 +1,18 @@
 package aoc;
-
 import java.io.IOException;
-import java.sql.Time;
-
-// import java.awt.datatransfer.Clipboard;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        System.out.println("Advent of Code 2024\n");
+        if (java.awt.GraphicsEnvironment.isHeadless()) System.err.println("Cannot access clipboard in headless environment");
+        
+        System.out.println("\nAdvent of Code 2024\n");
 
         DayOne dayOne = new DayOne();
-        System.out.println("Result A: " + dayOne.getResultA());
-        // System.out.println("Result B: " + dayOne.getResultB());
+        // System.out.println("\nResult A: " + dayOne.getResultA());
+        System.out.println("\nResult B: " + dayOne.getResultB());
 
-        if (java.awt.GraphicsEnvironment.isHeadless()) System.err.println("Cannot access clipboard in headless environment");
-
-        // clipboard will be empty on exit
         System.out.println("Press any key to exit...");
-        try {
-            System.in.read(); 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.in.read(); // clipboard will empty on exit 
         System.out.println("Exiting...");
-
     }
 }
--- aoc/app/src/main/java/aoc/DayOne.java
+++ aoc/app/src/main/java/aoc/DayOne.java
@@ -1,21 +1,24 @@
 package aoc;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.IntStream;
-import java.awt.datatransfer.StringSelection;
-import java.awt.Toolkit;
 
 public class DayOne {
+
     public int getResultA() throws IOException {
+        int total_distance;
         List<Integer> list_left = new ArrayList<>();
         List<Integer> list_right = new ArrayList<>();
         Scanner scanner = new Scanner(new File("aoc/app/src/main/resources/InputOne.txt"));
-        // Scanner scanner = new Scanner(new File("aoc/app/src/main/resources/SampleOne.txt"));
 
         while (scanner.hasNextLine()) {
             list_left.add(scanner.nextInt());
@@ -23,28 +26,55 @@ public class DayOne {
         }
         scanner.close();
 
-        if (list_left.size() != list_right.size()) throw new IllegalArgumentException("Lists must be of the same size");
+        if (list_left.size() != list_right.size()) {
+            throw new IllegalArgumentException("Lists must be of the same size");
+        }
 
         Collections.sort(list_left);
         Collections.sort(list_right);
 
-        int total_distance = IntStream.range(0, list_left.size())
+        total_distance = IntStream.range(0, list_left.size())
                 .map(index -> Math.abs(list_left.get(index) - list_right.get(index)))
                 .sum();
 
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(String.valueOf(total_distance)), null);
+        Toolkit.getDefaultToolkit().getSystemClipboard()
+                .setContents(new StringSelection(String.valueOf(total_distance)), null);
 
         return total_distance;
     }
 
     public int getResultB() throws IOException {
-        int result = 69;
-        Scanner scanner = new Scanner(new File("aoc/app/src/main/resources/SampleOne.txt"));
+        int similarity_score = 0;
+        List<Integer> list_left = new ArrayList<>();
+        List<Integer> list_right = new ArrayList<>();
+        Map<Integer, Integer> frequency_map = new HashMap<>();
+        Scanner scanner = new Scanner(new File("aoc/app/src/main/resources/InputOne.txt"));
+
         while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            System.out.println(input);
+            list_left.add(scanner.nextInt());
+            list_right.add(scanner.nextInt());
         }
         scanner.close();
-        return result;
+
+        if (list_left.size() != list_right.size()) {
+            throw new IllegalArgumentException("Lists must be of the same size");
+        }
+
+        for (int location_id : list_right) {
+            if (frequency_map.containsKey(location_id))
+                frequency_map.put(location_id, frequency_map.get(location_id) + 1);
+            else
+                frequency_map.put(location_id, 1);
+        }
+
+        for (int location_id : list_left) {
+            if (frequency_map.containsKey(location_id))
+                similarity_score += location_id * frequency_map.get(location_id);
+        }
+
+        Toolkit.getDefaultToolkit().getSystemClipboard()
+                .setContents(new StringSelection(String.valueOf(similarity_score)), null);
+
+        return similarity_score;
     }
 }
